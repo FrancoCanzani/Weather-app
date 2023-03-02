6b7d18bb38e9b68d04022b86b58f09a4633ff284
Franco Canzani <francocanzani@gmail.com>
2023-03-02 22:30:03 +0100

    Designed news section

--- src/news.js
+++ src/news.js
@@ -1,9 +1,8 @@
 const news = document.querySelector(".news");
 
-function newsDate(date) {
-  const pubDate = "2023-03-02 16:55:42";
+function newsDate(news_date) {
   const now = new Date();
-  const date = new Date(pubDate);
+  const date = new Date(news_date);
   const diff = (now.getTime() - date.getTime()) / 1000; // difference in seconds
 
   if (diff < 60) {
@@ -22,23 +21,30 @@ function getNews(data) {
   news.appendChild(sectionTitle);
 
   for (let index = 0; index < 5; index++) {
+    const newsContainer = document.createElement("div");
     const newsTitle = document.createElement("a");
     const newsCategory = document.createElement("h3");
-    const newsDescription = document.createElement("p");
+    // const newsDescription = document.createElement("p");
+    const news__date = document.createElement("h3");
 
+    newsContainer.classList.add("newsContainer");
     sectionTitle.classList.add("sectionTitle");
     newsTitle.classList.add("newsTitle");
     newsCategory.classList.add("newsCategory");
-    newsDescription.classList.add("newsDescription");
+    // newsDescription.classList.add("newsDescription");
+    news__date.classList.add("newsDate");
 
     newsTitle.innerText = data[3].results[index].title;
     newsTitle.href = data[3].results[index].link;
-    newsCategory.innerText = data[3].results[index].category[0];
-    newsDescription.innerText = data[3].results[index].description;
+    newsCategory.innerHTML = data[3].results[index].category[0];
+    // newsDescription.innerText = data[3].results[index].description;
+    news__date.innerText = newsDate(data[3].results[index].pubDate);
 
-    news.appendChild(newsTitle);
-    news.appendChild(newsCategory);
-    news.appendChild(newsDescription);
+    news.appendChild(newsContainer);
+    newsContainer.appendChild(newsCategory);
+    newsContainer.appendChild(newsTitle);
+    // news.appendChild(newsDescription);
+    newsContainer.appendChild(news__date);
   }
 }
 
